af472daf49018118788d4a23105ee0423257991e
LauraPineda <77401345+lauradesigner@users.noreply.github.com>
2024-03-03 21:35:53 -0500

    done

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import './App.css';
+import React, { useState, useCallback } from "react";
+import DurationExercise from "./components/DurationExercise";
+import RepetitionExercise from "./components/RepetitionExercise";
+
+const MENU_SCREEN = "menu";
+const EXERCISE_SCREEN = "exercise";
+const DURATION_EXERCISE = "duration";
+const REPETITION_EXERCISE = "repetition";
+
+const exerciseList = [
+  { type: DURATION_EXERCISE, name: "Running" },
+  { type: DURATION_EXERCISE, name: "Jumping" },
+  { type: DURATION_EXERCISE, name: "Swimming" },
+  { type: REPETITION_EXERCISE, name: "Push Ups" }
+];
+
+function App() {
+  const [currentScreen, setCurrentScreen] = useState(MENU_SCREEN);
+  const [currentExercise, setCurrentExercise] = useState(null);
+
+  const buttonClick = useCallback((exercise) => {
+    setCurrentExercise(exercise);
+    setCurrentScreen(EXERCISE_SCREEN);
+  }, [setCurrentExercise, setCurrentScreen]);
+
+  const setMenuScreen = useCallback(() => {
+    setCurrentScreen(MENU_SCREEN);
+    setCurrentExercise(null); // Reset the currentExercise when returning to the menu
+  }, [setCurrentScreen, setCurrentExercise]);
+
+  let screenComponent = null;
+
+  if (currentScreen === MENU_SCREEN) {
+    screenComponent = (
+      <div>
+        <p>Exercise Menu</p>
+        <ul>
+          {exerciseList.map(({ name, type }) => (
+            <li key={name}>
+              <button onClick={() => buttonClick({ name, type })}>{name}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else if (currentScreen === EXERCISE_SCREEN) {
+    switch (currentExercise?.type) {
+      case DURATION_EXERCISE:
+        screenComponent = (
+          <DurationExercise
+            exercise={currentExercise}
+            setMenuScreen={setMenuScreen}
+          />
+        );
+        break;
+      case REPETITION_EXERCISE:
+        screenComponent = (
+          <RepetitionExercise
+            exercise={currentExercise}
+            setMenuScreen={setMenuScreen}
+          />
+        );
+        break;
+      default:
+        // Handle unknown exercise type
+        screenComponent = null;
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{screenComponent}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DurationExercise.js
@@ -0,0 +1,17 @@
+import React from "react";
+import StopWatch from "./StopWatch";
+
+export default function DurationExercise({ exercise, setMenuScreen }) {
+  let { name } = exercise;
+
+  return (
+    <div>
+      <p>{name}</p>
+      <StopWatch />
+      <button onClick={() => setMenuScreen()}>Back to Menu</button>
+    </div>
+  );
+}
+
+
+
